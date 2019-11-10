ff7de9d14ffe3de4f395a16d4dda8ea35caf515e
Ezekiel <iamwebwiz@gmail.com>
2019-11-10 05:58:51 +0100

    Added initial views

--- meadowlark.js
+++ meadowlark.js
@@ -1,11 +1,47 @@
 const express = require('express');
+const handlebars = require('express-handlebars');
+const path = require('path');
 
 const app = express();
+const hbs = handlebars.create({ defaultLayout: 'main' });
 
 app.set('port', process.env.PORT || 3000);
 
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+app.get('/about', (req, res) => {
+  const fortunes = [
+    'Conquer your fears or they will conquer you.',
+    'Rivers need springs.',
+    "Do not fear what you don't know.",
+    'You will have a pleasant surprise.',
+    'Whenever possible, keep it simple.',
+  ];
+  let randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
+  res.render('about', { fortune: randomFortune });
+});
+
 app.use((req, res) => {
-  res.type('text/plain');
   res.status(404);
-  res.send('404 - Not found');
+  res.render('404');
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500);
+  res.render('500');
+});
+
+app.listen(app.get('port'), () => {
+  console.log(
+    `Server started at http://localhost:${app.get(
+      'port',
+    )} ; press Ctrl-C to terminate.`,
+  );
 });
